503601795b48559549ab076b19168e6772410632
lhk228 <lhk228@naver.com>
2024-08-05 10:13:07 +0900

    init

---
+++ event.js
@@ -0,0 +1,188 @@
+const READY_CHECK = {
+    data: { check: false },
+    copyImg: { check: false },
+    layer: { check: false, link: "" },
+    img: { check: false, link: "" },
+};
+
+$(function () {
+    const { year, pnu } = getQueryParams();
+    if (!year) {
+        init();
+    } else {
+        init(year, pnu);
+    }
+
+    $(`#BTN_PNU`).click(function () {
+        const pnu = $("#INPUT_PNU").val();
+        window.location.href = `index.html?year=${year}&pnu=${pnu}`;
+    });
+
+    $(`#BTN_VECTOR`).click(function () {
+        toggleVector();
+    });
+
+    $(`#BTN_YEAR`).click(function () {
+        const year = 2019;
+        window.location.href = `index.html?year=${year}&pnu=${pnu}`;
+    });
+
+    $(`#BTN_HIDE_UI`).click(function () {
+        $(`.search-box`).toggle();
+    });
+
+    $(`#BTN_DOWNLOAD`).click(function () {
+        convertCanvasImage("1");
+        convertCanvasImage("2");
+    });
+
+    $(`#BTN_COPY_CANVAS`).click(function () {
+        copyMapToCanvas("1");
+        copyMapToCanvas("2");
+    });
+
+    $(`#BTN_LAYER_DOWNLOAD`).click(function () {
+        convertLayerImage();
+    });
+});
+
+// URL의 쿼리 문자열을 읽어오는 함수
+function getQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    return {
+        year: params.get("year"),
+        pnu: params.get("pnu"),
+    };
+}
+
+//레이어 이미지 저장
+async function convertLayerImage() {
+    const link = await html2canvas(document.querySelector("#mapDiv1"), {
+        backgroundColor: null, // 투명 배경 설정
+    }).then((canvas) => {
+        // 캔버스를 이미지로 변환
+        var image = canvas.toDataURL("image/png");
+        if (!image) {
+            return false;
+        }
+        return image;
+    });
+    READY_CHECK.layer.link = link;
+
+    return true;
+}
+
+//맵 캔버스에 복사
+async function copyMapToCanvas(target) {
+    const mapContainer = document.getElementById("mapDiv" + target);
+    const mapCanvas = document.getElementById("mapCanvas" + target);
+    const mapContext = mapCanvas.getContext("2d");
+
+    // 지도 컨테이너의 크기로 캔버스 크기 설정
+    mapCanvas.width = mapContainer.offsetWidth;
+    mapCanvas.height = mapContainer.offsetHeight;
+
+    // 모든 캔버스 요소를 찾습니다
+    const canvases = mapContainer.querySelectorAll(".olTileImage");
+
+    // Promise.all을 사용하여 모든 이미지 처리를 병렬로 수행
+    await Promise.all(
+        Array.from(canvases).map(async (canvas) => {
+            const imgUrl = canvas.src;
+            try {
+                const result = await convertImgToBase64({ imgUrl });
+                const base64 = result.data.imgBase64;
+
+                // base64를 이미지로 변환
+                const img = new Image();
+                img.src = "data:image/png;base64," + base64;
+
+                await new Promise((resolve) => {
+                    img.onload = () => {
+                        // 캔버스의 위치 정보를 가져옵니다
+                        const rect = canvas.getBoundingClientRect();
+                        const containerRect = mapContainer.getBoundingClientRect();
+
+                        // 컨테이너를 기준으로 한 상대적 위치 계산
+                        const x = rect.left - containerRect.left;
+                        const y = rect.top - containerRect.top;
+
+                        // 새 캔버스에 이미지 그리기
+                        mapContext.drawImage(img, x, y, canvas.width, canvas.height);
+                        resolve();
+                    };
+                });
+            } catch (error) {
+                console.error("이미지 변환 중 오류 발생:", error);
+                return false;
+            }
+        })
+    );
+    return true;
+}
+
+//base64 이미지로 변환(세탁)
+function convertImgToBase64(url) {
+    const result = axios.post("http://127.0.0.1:3000/baseImg", url);
+    return result;
+}
+
+//캔버스 이미지 변환
+async function convertCanvasImage(target) {
+    var canvas = document.getElementById("mapCanvas" + target);
+
+    // canvas를 이미지로 변환
+    let imageDataURL;
+
+    try {
+        imageDataURL = canvas.toDataURL("image/png");
+    } catch (e) {
+        console.log("캔버스 이미지 변환 실패:", e);
+        return false;
+    }
+    READY_CHECK.img.link = imageDataURL;
+
+    return true;
+}
+
+//이미지 다운로드 동기처리
+let layerImageDownloaded = false; // 레이어 이미지 다운로드 여부를 추적하는 전역 변수
+async function handleMapDownload(target) {
+    try {
+        const checkCopy = await copyMapToCanvas(target);
+        const checkImage = await convertCanvasImage(target);
+
+        // 레이어 이미지 변환 (한 번만 수행)
+        if (!layerImageDownloaded) {
+            const checkLayer = await convertLayerImage();
+            READY_CHECK.layer.check = checkLayer;
+        }
+
+        READY_CHECK.copyImg.check = checkCopy;
+        READY_CHECK.img.check = checkImage;
+
+        let isAllReady = Object.values(READY_CHECK).every((item) => item.check === true);
+
+        // isAllReady = false;
+
+        if (isAllReady === true) {
+            $(`[name='img-download']`).text("S");
+            // var canvasDownload = document.createElement("a");
+            // canvasDownload.download = "canvas-image.png"; // 다운로드할 파일 이름
+            // canvasDownload.href = READY_CHECK.img.link;
+            // canvasDownload.click();
+
+            // // 레이어 이미지 다운로드 (아직 다운로드하지 않았을 경우에만)
+            // if (!layerImageDownloaded) {
+            //     const layerDownload = document.createElement("a");
+            //     layerDownload.download = "layer.png";
+            //     layerDownload.href = READY_CHECK.layer.link; // 레이어 이미지 URL을 저장했다고 가정
+            //     layerDownload.click();
+            //     layerImageDownloaded = true; // 레이어 이미지 다운로드 완료 표시
+            // }
+        }
+    } catch (error) {
+        $(`['img-download']`).text("F");
+        console.error("맵 다운로드 중 오류 발생:", error);
+    }
+}
---
+++ map.js
@@ -0,0 +1,256 @@
+const apiKey = "Fj6bWPPoqYIcDAna3Bce";
+const domain = "http://127.0.0.1:5500";
+var map;
+var map2;
+var mapBounds = new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34);
+var mapMinZoom = 7;
+var mapMaxZoom = 20;
+var xyString;
+var reqUrl;
+var mtype = "AIR";
+
+//영역 레이어 생성
+function makeLayer(year) {
+    var layer = new OpenLayers.Layer.XYZ(mtype, "", {
+        url: farmmapObj.tmsProxyUrl,
+        layer: mtype,
+        matrixSet: "NGIS_AIR",
+        format: "image/jpg",
+        style: "_null",
+        transitionEffect: "resize",
+        serverResolutions: farmmapObj.oResolutions,
+        opacity: 1,
+        tileSize: new OpenLayers.Size(256, 256),
+        tileOrigin: new OpenLayers.LonLat(-200000.0, 4000000.0),
+        maxExtent: new OpenLayers.Bounds(-200000.0, 997738.4107, 2802261.589, 4000000.0),
+        transparent: true,
+        buffer: 1,
+        getURL: function (bounds) {
+            var res = this.getServerResolution();
+            var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
+            var y = Math.round((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
+            var z = this.getServerZoom() + 5;
+            if (x >= 0 && y >= 0) {
+                return (
+                    this.options.url +
+                    "?service=" +
+                    "WMTS" +
+                    "&request=" +
+                    "GetTile" +
+                    "&version=" +
+                    "1.0.0" +
+                    "&layer=" +
+                    `AIRPHOTO_${year}` +
+                    "&style=" +
+                    this.style +
+                    "&format=" +
+                    this.format +
+                    "&tilematrixset=" +
+                    this.matrixSet +
+                    "&tilematrix=" +
+                    z +
+                    "&tilerow=" +
+                    y +
+                    "&tilecol=" +
+                    x +
+                    "&apiKey=" +
+                    farmmapObj.airLayerApiKey +
+                    "&baseUrl=" +
+                    farmmapObj.airLayerUrl
+                );
+            } else {
+                return "../images/nodata.png";
+            }
+        },
+        isBaseLayer: true,
+        isFirstLayer: true,
+        visibility: true,
+    });
+    return layer;
+}
+
+//맵 생성
+function init(year = "2022", pnu = "3611031024201550000") {
+    if (!map) {
+        map = farmmapObj.init("mapDiv1");
+        map2 = farmmapObj.init("mapDiv2");
+    }
+    reqUrl = farmmapObj.rootUri;
+
+    const layer = makeLayer(year);
+    const layer2 = makeLayer(year - 1);
+
+    map.addLayer(layer);
+    map2.addLayer(layer2);
+
+    getFarmmapDataSeachPnu(pnu);
+}
+
+//pnu코드로 데이터 조회
+async function getFarmmapDataSeachPnu(pnu) {
+    var params = {};
+    params.pnu = pnu;
+    params.mapType = "farmmap";
+    params.columnType = "KOR";
+    params.apiKey = apiKey;
+    params.domain = domain;
+    params.apiVersion = "v1";
+    farmmapObj.removeLayer("vectorLayer", map);
+    const data = await fetchData("farmmapApi/getFarmmapDataSeachPnu.do", params);
+
+    addVector(data);
+
+    setTimeout(async () => {
+        await handleMapDownload("1");
+        await handleMapDownload("2");
+        // convertLayerImage();
+    }, 2000);
+}
+
+//ajax 호출
+async function fetchData(url, params) {
+    const res = await axios({
+        url: reqUrl + url,
+        adapter: axiosJsonpAdapter,
+        params: params,
+        method: "GET",
+        cache: false,
+    })
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            console.error("Request failed", error);
+            $.unblockUI();
+        });
+    return res;
+}
+
+//벡터 레이어 추가
+function addVector(data) {
+    if (data == null || data?.output.farmmapData.count === 0) {
+        READY_CHECK.data.check = false;
+        return;
+    }
+
+    READY_CHECK.data.check = true;
+    var layerName = "vectorLayer";
+    var layerOption = {
+        hover: false,
+        multiple: false,
+        toggle: true,
+    };
+    farmmapObj.addVectorLayer(layerName, layerOption, map);
+    farmmapObj.addVectorLayer(layerName, layerOption, map2);
+
+    var farmmapData;
+    if (data.output.farmmapData != null) {
+        farmmapData = data.output.farmmapData;
+        for (var k = 0; k < farmmapData.data.length; k++) {
+            if (k > 300) break;
+
+            var feature = farmmapData.data[k];
+            var xy = feature.geometry[0].xy;
+
+            var id = "";
+            var label = "";
+
+            if (data.input.mapType == "base") {
+                if (data.input.columnType.toUpperCase() == "KOR") {
+                    id = feature.필지고유번호;
+                    label = feature.필지고유번호;
+                } else {
+                    id = feature.pnu;
+                    label = feature.pnu;
+                }
+            } else {
+                if (data.input.columnType.toUpperCase() == "KOR") {
+                    id = feature.팜맵ID;
+                    label = feature.팜맵ID;
+                } else {
+                    id = feature.id;
+                    label = feature.id;
+                }
+            }
+
+            vectorOptions = {
+                id: id.toString(),
+                type: "polygon",
+                xy: xy,
+                data: feature,
+                style: {
+                    fillColor: "#ff0000",
+                    fillOpacity: 0.5,
+                    strokeWidth: 1,
+                    strokeColor: "#ff0000",
+                    strokeLinecap: "round",
+                    // fontSize: "12px",
+                    // fontColor: "black",
+                    // fontWeight: "bold",
+                    // label: label.toString(),
+                    // labelOutlineColor: "#ffffff",
+                    // labelOutlineWidth: 1,
+                },
+            };
+            farmmapObj.addVector("vectorLayer", vectorOptions, map);
+            farmmapObj.addVector("vectorLayer", vectorOptions, map2);
+        }
+    }
+
+    if (farmmapObj.getObject("layer", "vectorLayer", map).features.length > 0) {
+        map.zoomToExtent(farmmapObj.getObject("layer", "vectorLayer", map).features[0].geometry.getBounds());
+        map.zoomTo(15);
+        map2.zoomToExtent(farmmapObj.getObject("layer", "vectorLayer", map2).features[0].geometry.getBounds());
+        map2.zoomTo(15);
+    }
+}
+
+//벡터 토글
+function toggleVector() {
+    var layer = farmmapObj.getObject("layer", "vectorLayer", map);
+    if (layer) {
+        $(`#${layer.id}`).toggle();
+    }
+}
+
+//벡터 스타일 설정
+function setVectorStyle(type, vecId) {
+    var vec = farmmapObj.getObject("vector", vecId, map1);
+    var styleOptions = {};
+    if (vec != null) {
+        if (type == "point") {
+            styleOptions = {
+                fillColor: "#ff99ee",
+                graphicName: "x",
+                pointRadius: 20,
+                strokeWidth: 4,
+                strokeColor: "#e62222",
+                fontColor: "#ffffff",
+                fontSize: "12px",
+                fontWeight: "bold",
+                label: vec.id + "스타일변경",
+                labelYOffset: -30,
+                labelOutlineColor: "black",
+                labelOutlineWidth: 10,
+                labelOutlineOpacity: 0.7,
+            };
+            farmmapObj.setVectorStyle(vec, styleOptions);
+        } else if (type == "lineString") {
+            styleOptions = {
+                strokeWidth: 10,
+                strokeColor: "blue",
+                fontColor: "#000000",
+                fontSize: "20px",
+                fontWeight: "bold",
+                label: vec.id + "스타일변경",
+                labelYOffset: -30,
+                labelOutlineColor: "white",
+                labelOutlineWidth: 10,
+                labelOutlineOpacity: 0.7,
+            };
+            farmmapObj.setVectorStyle(vec, styleOptions);
+        }
+    } else {
+        alert(vecId + " 아이디의 벡터가 없습니다.");
+    }
+}
